f8b93875d3de239eb57e98490fb207ff920857df
Ismarji Workman <ismarjiw@protonmail.com>
2022-11-28 18:22:36 -0500

    second commit

--- project-tracker/project-tracker.sql
+++ project-tracker/project-tracker.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.8 (Ubuntu 13.8-1.pgdg20.04+1)
--- Dumped by pg_dump version 13.8 (Ubuntu 13.8-1.pgdg20.04+1)
+-- Dumped from database version 14.5 (Homebrew)
+-- Dumped by pg_dump version 14.5 (Homebrew)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,7 +21,43 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: projects; Type: TABLE; Schema: public; Owner: bdo_ani
+-- Name: grades; Type: TABLE; Schema: public; Owner: Jahziel
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO "Jahziel";
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: Jahziel
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO "Jahziel";
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: Jahziel
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
+-- Name: projects; Type: TABLE; Schema: public; Owner: Jahziel
 --
 
 CREATE TABLE public.projects (
@@ -31,10 +67,10 @@ CREATE TABLE public.projects (
 );
 
 
-ALTER TABLE public.projects OWNER TO bdo_ani;
+ALTER TABLE public.projects OWNER TO "Jahziel";
 
 --
--- Name: students; Type: TABLE; Schema: public; Owner: bdo_ani
+-- Name: students; Type: TABLE; Schema: public; Owner: Jahziel
 --
 
 CREATE TABLE public.students (
@@ -44,10 +80,29 @@ CREATE TABLE public.students (
 );
 
 
-ALTER TABLE public.students OWNER TO bdo_ani;
+ALTER TABLE public.students OWNER TO "Jahziel";
 
 --
--- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: bdo_ani
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: Jahziel
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: Jahziel
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Blockly	100
+4	sdevelops	Markov	50
+\.
+
+
+--
+-- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: Jahziel
 --
 
 COPY public.projects (title, description, max_grade) FROM stdin;
@@ -57,7 +112,7 @@ Blockly	Programmatic Logic Puzzle Game	100
 
 
 --
--- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: bdo_ani
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: Jahziel
 --
 
 COPY public.students (github, first_name, last_name) FROM stdin;
@@ -67,7 +122,22 @@ sdevelops	Sarah	Developer
 
 
 --
--- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: bdo_ani
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: Jahziel
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 4, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: Jahziel
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: Jahziel
 --
 
 ALTER TABLE ONLY public.projects
@@ -75,11 +145,27 @@ ALTER TABLE ONLY public.projects
 
 
 --
--- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: bdo_ani
+-- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: Jahziel
 --
 
 ALTER TABLE ONLY public.students
     ADD CONSTRAINT students_pkey PRIMARY KEY (github);
+
+
+--
+-- Name: grades grades_project_title_fkey; Type: FK CONSTRAINT; Schema: public; Owner: Jahziel
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_project_title_fkey FOREIGN KEY (project_title) REFERENCES public.projects(title);
+
+
+--
+-- Name: grades grades_student_github_fkey; Type: FK CONSTRAINT; Schema: public; Owner: Jahziel
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_student_github_fkey FOREIGN KEY (student_github) REFERENCES public.students(github);
 
 
 --
